d8331364ed6d437135695767558ed6e10223d7ad
Ghady Abboud <abboudghady@gmail.com>
2025-04-11 22:31:06 -0400

    Added simSpace unit tests

--- demo/src/main/java/com/EMPhysics/SimulationSpace.java
+++ demo/src/main/java/com/EMPhysics/SimulationSpace.java
@@ -1,13 +1,26 @@
 package com.EMPhysics;
 
+import java.util.ArrayList;
+
 public class SimulationSpace {
-    private int numberOfCharges;
+    private ArrayList<ChargedParticle> charges;
+    private ChargedParticle charge = new ChargedParticle(new Vector2D(0, 0), new Vector2D(0, 0), 10, 1e-6);
+
     
     public SimulationSpace() {
-        numberOfCharges = 1;
+        charges = new ArrayList<>();
+        charges.add(charge);
     }
 
-    public int getNumberOfCharges() {
-        return numberOfCharges;
+    public ArrayList<ChargedParticle> getCharges() {
+        return charges;
+    }
+
+    public void addCharge(ChargedParticle charge) {
+        charges.add(charge);
+    }
+
+    public void removeCharge(ChargedParticle charge) {
+        charges.remove(charge);
     }
 }
--- demo/src/test/java/com/EMPhysics/SimulationSpaceTest.java
+++ demo/src/test/java/com/EMPhysics/SimulationSpaceTest.java
@@ -8,15 +8,43 @@ import org.junit.jupiter.api.Test;
 public class SimulationSpaceTest {
 
     SimulationSpace simSpace;
+    ChargedParticle charge2;
 
     @BeforeEach
     public void setUp() {
         simSpace = new SimulationSpace();
+        charge2 = new ChargedParticle(new Vector2D(0, 0), new Vector2D(0, 0), 10, 1e-6);
+    }
+    @Test
+    public void simulation_space_created_with_one_charge_by_default() {
+        assertEquals(1, simSpace.getCharges().size());
     }
 
     @Test
-    public void simulation_space_created_with_one_charge_by_default() {
-        assertEquals(1, simSpace.getNumberOfCharges());
+    public void simulation_space_correctly_retrieves_all_charges() {
+        simSpace.addCharge(charge2);
+
+        assertEquals(2, simSpace.getCharges().size());
+    }
+
+    @Test
+    public void simulation_space_correctly_adds_charge() {
+        simSpace.addCharge(charge2);
+
+        assertEquals(2, simSpace.getCharges().size());
+    }
+
+    @Test
+    public void simulation_space_correctly_removes_charge() {
+        simSpace.addCharge(charge2);
+        simSpace.removeCharge(charge2);
+
+        assertEquals(1, simSpace.getCharges().size());
+    }
+
+    @Test
+    public void simulation_space_correctly_updates_properties() {
+        
     }
 
 
