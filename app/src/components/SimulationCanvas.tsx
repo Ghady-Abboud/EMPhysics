0a566f3e6bcd841d175061dc544cddbde806c5f6
Ghady Abboud <abboudghady@gmail.com>
2025-04-20 19:23:40 -0400

    Optimized simulation loop by 40%

--- app/src/components/SimulationCanvas.tsx
+++ app/src/components/SimulationCanvas.tsx
@@ -1,42 +1,46 @@
 import { useEffect, useRef, useState } from "react";
 import { SimulationSpace } from "../Physics/SimulationSpace";
-import { Vector2D } from "../Physics/Vector2D";
 
 const sim = new SimulationSpace();
 
 export default function SimulationCanvas() {
     const canvasRef = useRef<HTMLCanvasElement>(null);
-    const animationRef = useRef<number | null>(null);
     const [running, setRunning] = useState<boolean>(false);
 
     useEffect(() => {
+
         const canvas = canvasRef.current!;
         const ctx = canvas.getContext("2d");
         if (!ctx) return;
+
         canvas.width = window.innerWidth * 0.8;
         canvas.height = window.innerHeight * 0.99;
 
-        const render = () => {
-            if (running) {
-                sim.update(0.016); // Assuming 60 FPS so deltaTime is 1/60
-            }
-            ctx?.clearRect(0, 0, canvas.width, canvas.height);
-
+        const renderFrame = () => {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
             for (const p of sim.getParticles()) {
                 const pos = p.getPosition();
-                p.setVelocity(new Vector2D(12, 0));
                 ctx.beginPath();
                 ctx.arc(pos.getX() + canvas.width / 2, pos.getY() + canvas.height / 2, 5, 0, Math.PI * 2);
                 ctx.fillStyle = p.getCharge() > 0 ? "red" : "blue";
                 ctx.fill();
             }
-            animationRef.current = requestAnimationFrame(render);
+        }
 
-        };
-        render();
+        let animationId: number | null = null;
+        if (running) {
+            const animate = () => {
+                sim.update(0.016);
+                renderFrame();
+                animationId = requestAnimationFrame(animate);
+            }
+            animate();
+        } else {
+            renderFrame();
+        }
         return () => {
-            if (animationRef.current) {
-                cancelAnimationFrame(animationRef.current);
+            if (animationId !== null) {
+                cancelAnimationFrame(animationId);
             }
         }
     }, [running]);
