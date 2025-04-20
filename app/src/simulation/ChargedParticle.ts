1e19c13f6fab8b41c35efebb6f932f51ccd38b0d
Ghady Abboud <abboudghady@gmail.com>
2025-04-19 22:19:58 -0400

    Added ChargedParticle in ts

---
+++ app/src/simulation/ChargedParticle.ts
@@ -0,0 +1,54 @@
+import { Vector2D } from "./Vector2D";
+
+export class ChargedParticle {
+    private position: Vector2D;
+    private velocity: Vector2D;
+    private charge: number;
+    private mass: number;
+
+    constructor(position: Vector2D, velocity: Vector2D, charge: number, mass: number) {
+        this.position = position;
+        this.velocity = velocity;
+        this.charge = charge;
+        this.mass = mass;
+    }
+
+    public getPosition(): Vector2D {
+        return this.position;
+    }
+
+    public getVelocity(): Vector2D {
+        return this.velocity;
+    }
+
+    public getCharge(): number {
+        return this.charge;
+    }
+
+    public getMass(): number {
+        return this.mass;
+    }
+
+    public setPosition(position: Vector2D): void {
+        this.position = position;
+    }
+
+    public setVelocity(velocity: Vector2D): void {
+        this.velocity = velocity;
+    }
+
+    public setMass(mass: number): void {
+        this.mass = mass;
+    }
+
+    public setCharge(charge: number): void {
+        this.charge = charge;
+    }
+
+    public updatePosition(deltaTime: number): void {
+        this.setPosition(this.position.add(this.velocity.scalar_multiply(deltaTime)));
+    }
+    public updateVelocity(acceleration: Vector2D, deltaTime: number): void {
+        this.setVelocity(this.velocity.add(acceleration.scalar_multiply(deltaTime)));
+    }
+}
